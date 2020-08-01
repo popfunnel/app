6592810a963fd815f2d6f4c2061da7a18bc9b782
curtislin7 <curtislin7@gmail.com>
2020-08-01 17:00:17 -0600

    Add localhost proxy in client

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+dotenv.config()
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port)
