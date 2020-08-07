5f3d08b27276268b23b2171e370a484d20c87b23
curtislin7 <curtislin7@gmail.com>
2020-08-06 22:59:10 -0600

    Add fonts folder to client/src

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css';
-
+import './fonts/FiraSans-Regular.ttf'
 
 const routing = (
     <BrowserRouter>
