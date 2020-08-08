580fbd908d40f708b91558c6bdfc639f72184217
curtislin7 <curtislin7@gmail.com>
2020-08-08 16:56:23 -0600

    Reference user inside controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,12 +1,12 @@
 const User = require('../models').User;
 
 module.exports = {
-    create(req, res) {
-      return Todo
-        .create({
-          emailAddress: req.body.emailAddress,
-        })
-        .then(todo => res.status(201).send(todo))
-        .catch(error => res.status(400).send(error));
-    },
-  };
+  create(req, res) {
+    return User
+      .create({
+        emailAddress: req.body.emailAddress,
+      })
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error));
+  },
+};
--- server/models/index.js
+++ server/models/index.js
@@ -11,9 +11,12 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
-    config.database, config.username, config.password, config
+    config.database,
+    config.username,
+    config.password,
+    config
   );
-}
+};
 
 fs
   .readdirSync(__dirname)
