56874c5d37701a7682340ab860be131a5c1eddb7
curtislin7 <curtislin7@gmail.com>
2020-08-09 11:36:31 -0600

    Change default db to popfunnel_development

--- models/psql_pool.js
+++ models/psql_pool.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 const config = {
     // Required
-    // url: 'jdbc:postgresql://localhost/popfunnel_development',
-    url: 'jdbc:postgresql://localhost/db-visuals-pg',
+    url: 'jdbc:postgresql://localhost/popfunnel_development',
+    // url: 'jdbc:postgresql://localhost/db-visuals-pg',
     // Optional
     drivername: 'org.postgresql.Driver',
     minpoolsize: 10,
