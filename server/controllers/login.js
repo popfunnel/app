540245bd3219bc55965d90d0b8fc027b253286c4
Austin Gibbons <austin@fullvetting.com>
2020-08-13 21:39:58 -0500

    sketch routing for login

---
+++ server/controllers/login.js
@@ -0,0 +1,10 @@
+// todo?
+
+module.exports = {
+    get(req, res) {
+        console.log("Asking to get login")
+    },
+    post(req, res) {
+        console.log("Asking to post login")
+    },
+};
