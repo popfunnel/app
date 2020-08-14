0328ed87c54926a6d1974754833b53b991f46006
austinbgibbonsdb <69223026+austinbgibbonsdb@users.noreply.github.com>
2020-08-13 21:54:30 -0500

    sketch routing for login (#4)

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,25 @@ import { Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/core/styles';
 import { QueryPage } from './pages/query/QueryPage'
 import { AboutPage } from './pages/AboutPage'
+import { LoginPage } from './pages/LoginPage'
 import { NavBar } from './components/navigation/NavBar'
 import { theme } from './AppTheme';
+
+const NavContent = () => {
+  return (
+    <NavBar>
+      <Route exact path="/" component={QueryPage}/>
+    </NavBar>
+  )
+}
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-        <NavBar>
-            <Switch>
-                <Route exact path="/" component={QueryPage}/>
-                <Route path="/about" component={AboutPage}/>
-            </Switch>
-        </NavBar>
+        <Switch>
+                <Route exact path="/" component={NavContent}/>
+                <Route path="/login" component={LoginPage}/>
+        </Switch>
     </ThemeProvider>
   )
 }
