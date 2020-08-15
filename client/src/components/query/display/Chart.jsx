9292ef827354585806def55f4a02f517e80466f9
curtislin7 <curtislin7@gmail.com>
2020-08-14 19:35:29 -0600

    Add proof of concept for nivo

--- client/src/components/query/display/Chart.jsx
+++ client/src/components/query/display/Chart.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {FlexibleXYPlot, XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalBarSeries} from 'react-vis';
+import { ResponsiveBar } from '@nivo/bar'
+
 import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-      height: '100%',
-      width: '100%',
-    },
-    control: {
-      padding: theme.spacing(2),
-    }
-}));
+// const useStyles = makeStyles((theme) => ({
+//     paper: {
+//       height: '100%',
+//       width: '100%',
+//     },
+//     control: {
+//       padding: theme.spacing(2),
+//     }
+// }));
 
-export const Chart = ({queryResults, seriesType}) => {
-    const classes = useStyles();
+export const BarChart = ({queryResults, seriesType}) => {
+    // const classes = useStyles();
     let attributes = queryResults.length ? Object.keys(queryResults[0]) : [];
     let data = queryResults.map(item => {
         return {
@@ -24,23 +25,36 @@ export const Chart = ({queryResults, seriesType}) => {
     })
 
     return (
-            <div style={{display:'flex', justifyContent:'center', height:'85%', width:'100%'}}>
-                <Paper style={{height:'300px', width:'300px'}}>
-                    <FlexibleXYPlot xType={'ordinal'}>
-                        <HorizontalGridLines/>
-                        <XAxis
-                            style={{
-                                fontSize:'12px'
-                            }}
-                        />
-                        <YAxis
-                            
-                        />
-                        <VerticalBarSeries
-                            data={data}
-                            color={'blue'}
-                        />
-                    </FlexibleXYPlot>
+            <div style={{display:'flex', alignItems: 'center', justifyContent:'center', height:'85%', width:'100%'}}>
+                <Paper style={{height:'80%', width:'80%'}}>
+                    <ResponsiveBar
+                        indexBy={'name'}
+                        keys={['count']}
+                        data={queryResults}
+                        colors={{ scheme: 'category10' }}
+                        margin={{
+                            "top": 50,
+                            "right": 60,
+                            "bottom": 50,
+                            "left": 60
+                        }}
+                        axisBottom={{
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                            legend: 'Genre',
+                            legendPosition: 'middle',
+                            legendOffset: 32
+                        }}
+                        axisLeft={{
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                            legend: 'food',
+                            legendPosition: 'middle',
+                            legendOffset: -40
+                        }}
+                    />
                 </Paper> 
             </div>
     );
