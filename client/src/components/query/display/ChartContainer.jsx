f21276e89218b77964c80133ee848481a7b33458
curtislin7 <curtislin7@gmail.com>
2020-09-16 12:07:13 -0600

    Remove unused code and comments

--- client/src/components/query/display/ChartContainer.jsx
+++ client/src/components/query/display/ChartContainer.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
 // TODO create index.js file to import all these guys
 import { ConnectedResultsTable } from './Table'
 import { ConnectedCustomBarChart } from './Bar'
 import { ConnectedCustomLineChart } from './Line'
-import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux'
 import { asOutputContainer }  from './OutputContainer';
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        margin: 'auto',
-        height: '100%',
-        width: '100%',
-    },
-    control: {
-      padding: theme.spacing(2),
-    }
-}));
-
 const ChartContainer = ({queryResults, seriesType, config}) => {
-    const classes = useStyles();
-
-    // TODO: create fixed chart container(Paper) with overflow auto
-    // TODO: output dropdown should be fixed size with overflow auto
     // TODO: add row numbers to table, headers should be styled
 
     const getChart = () => {
