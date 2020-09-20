c577ba609a76c57443f6fe224830fc5168a5f99f
curtislin7 <curtislin7@gmail.com>
2020-09-20 15:10:51 -0600

    Disable save button in QueryTool page if empty chart name

--- client/src/components/query/actions/QueryToolHeader.jsx
+++ client/src/components/query/actions/QueryToolHeader.jsx
@@ -102,6 +102,7 @@ export const QueryToolHeader = ({currentDashboardId, refreshDashboardInfo, saveC
                             }
                         }}
                         disableRipple
+                        disabled={!chartName.length ? true : false}
                     >
                         Save
                     </Button>
