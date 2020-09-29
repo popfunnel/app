d6177605553abe25bc92d7a17a631335a52a589d
Austin Gibbons <austin@fullvetting.com>
2020-09-29 10:23:04 -0500

    add customer model, controller, and routing

--- sequelize/controllers/index.js
+++ sequelize/controllers/index.js
@@ -2,10 +2,12 @@ const dashboards = require('./dashboards');
 const users = require('./users');
 const charts = require('./charts');
 const databases = require('./databases');
+const customers = require('./customers');
 
 module.exports = {
     dashboards,
     users,
     charts,
-    databases
+    databases,
+    customers
 };
