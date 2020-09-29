8e9fbdc9381ae3288e2f3642c07542ee43ed7bca
Austin Gibbons <austin@fullvetting.com>
2020-09-29 08:56:46 -0500

    add routing for database controller

--- sequelize/controllers/index.js
+++ sequelize/controllers/index.js
@@ -1,9 +1,11 @@
 const dashboards = require('./dashboards');
 const users = require('./users');
 const charts = require('./charts');
+const databases = require('./databases');
 
 module.exports = {
     dashboards,
     users,
-    charts
+    charts,
+    databases
 };
