9a357c68080722139d4c83cd48bfc0009f081f4e
Austin Gibbons <austin@fullvetting.com>
2020-09-29 08:44:42 -0500

    add database route and controller

---
+++ routes/database.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const databasesController = require('../sequelize/controllers').databases;
+const router = new express.Router();
+
+router.get('/list', databasesController.list);
+
+router.post('/create', databasesController.create);
+
+router.get('/info/:database_id', databasesController.getById);
+
+module.exports = router;
