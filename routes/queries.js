9b671ebda768a0bc0040afcc05d5a4723c41237a
curtislin7 <curtislin7@gmail.com>
2021-01-22 22:49:33 -0700

    Add mock for SchemaTreeView with example db schemas info

--- routes/queries.js
+++ routes/queries.js
@@ -3,7 +3,7 @@ const psql_pool = require('../models/psql_pool');
 
 const router = new express.Router();
 
-router.get('/schema', function(req, res, next) {
+router.get('/schemas', function(req, res, next) {
     const query = `select table_schema,
     table_name,
     ordinal_position as position,
